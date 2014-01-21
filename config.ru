95edbeb1dd2057e45b263654e1c7bde1f02807a7
Brandur <brandur@mutelight.org>
2014-01-21 00:02:57 -0800

    Assets and the start of a design

--- config.ru
+++ config.ru
@@ -3,3 +3,11 @@ require "./web"
 Slim::Engine.set_default_options format: :html5, pretty: true
 
 run SchemaDoc::Web
+run Rack::Builder.new {
+  use Rack::Instruments
+  use Rack::Deflater
+  run Sinatra::Router.new {
+    mount SchemaDoc::Assets
+    run SchemaDoc::Web
+  }
+}
--- web.rb
+++ web.rb
@@ -15,8 +15,58 @@ module SchemaDoc
       ENV["SCHEMA_URL"] || raise("missing=SCHEMA_URL")
     end
 
+    def self.production?
+      ENV["RACK_ENV"] == "production"
+    end
+
+    def self.release
+      ENV["RELEASE"] || "1"
+    end
+
     def self.root
       @root ||= File.expand_path("../", __FILE__)
+    end
+  end
+
+  class Assets < Sinatra::Base
+    def initialize(*args)
+      super
+      path = "#{Config.root}/assets"
+      @assets = Sprockets::Environment.new do |env|
+        Slides.log :assets, path: path
+
+        env.append_path(path + "/images")
+        env.append_path(path + "/javascripts")
+        env.append_path(path + "/stylesheets")
+
+        if Config.production?
+          env.js_compressor  = YUI::JavaScriptCompressor.new
+          env.css_compressor = YUI::CssCompressor.new
+        end
+      end
+    end
+
+    get "/assets/:release/app.css" do
+      respond_with_asset(@assets["app.css"])
+    end
+
+    get "/assets/:release/app.js" do
+      respond_with_asset(@assets["app.js"])
+    end
+
+    %w{jpg png}.each do |format|
+      get "/assets/:image.#{format}" do |image|
+        respond_with_asset(@assets["#{image}.#{format}"])
+      end
+    end
+
+    private
+
+    def respond_with_asset(asset)
+      cache_control(:public, max_age: 2592000)
+      content_type(asset.content_type)
+      last_modified(asset.mtime.utc)
+      asset
     end
   end
 
