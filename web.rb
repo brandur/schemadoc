f2e56de1551cfab0bc1382f8a9595489f2fda55e
Brandur <brandur@mutelight.org>
2014-01-20 22:46:10 -0800

    Working attributes

--- web.rb
+++ web.rb
@@ -35,18 +35,44 @@ module SchemaDoc
     get "/*" do
       path = params[:splat][0]
       @type_schema = schema[path] || halt(404)
+      @attributes = build_attributes(@type_schema)
       slim :show
+    end
+
+    def build_attributes(type_schema)
+      attributes = []
+      type_schema["properties"].each do |name, attrs|
+        ref = attrs["$ref"] ? schema.find(attrs["$ref"]) : nil
+        if ref
+          attributes << { "name" => name }.merge(ref)
+        else
+          first = true
+          attrs["properties"].each do |subname, subattrs|
+            subref = subattrs["$ref"] ? schema.find(subattrs["$ref"]) : {}
+            attribute = { "name" => subname, "subobject" => true }.merge(subref)
+            if first
+              attribute.merge!({
+                "supername" => name,
+                "count" => attrs["properties"].count
+              })
+              first = false
+            end
+            attributes << attribute
+          end
+        end
+      end
+      attributes
     end
 
     def schema
       @@schema
     end
 
-    def type(attrs)
-      return "" if !attrs || !attrs["type"]
-      types = attrs["type"].dup.reject { |t| t == "null" }.join("/")
-      type = attrs["format"] || types
-      if attrs["type"].include?("null")
+    def type(attribute)
+      return "" if !attribute || !attribute["type"]
+      types = attribute["type"].dup.reject { |t| t == "null" }.join("/")
+      type = attribute["format"] || types
+      if attribute["type"].include?("null")
         "nullable #{type}"
       else
         type
